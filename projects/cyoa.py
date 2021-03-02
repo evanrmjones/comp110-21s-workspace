6b2f5e73bf8121878bc5b1454436f4e5fa039768
Evan Jones <ermjones@email.unc.edu>
2021-03-01 21:00:47 -0500

    ohh

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -39,7 +39,7 @@ def greet() -> None:
     return None 
 
 
-def rand_flip(list: list) -> list: 
+def rand_flip(list: list[str]) -> list: 
     """Flips coin randomly."""
     index_list: int = random.randint(0, 1)
     return list[index_list]
